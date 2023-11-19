c6398a41fbd6a5b8baccc0420a3328ed5d9701c1
kirill unshchikov <kaunshch@mts.ru>
2023-11-19 19:43:07 +0300

    4.1 - kafka logger

---
+++ internal/logger/kafka.go
@@ -0,0 +1,42 @@
+package logger
+
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+)
+
+//var _ kafka.Logger = (*KafkaAdapted)(nil)
+
+type KafkaOptions struct {
+}
+
+type KafkaAdapted struct {
+	Logger     *zap.Logger `option:"mandatory"`
+	isErrorLvl bool
+}
+
+func (k *KafkaAdapted) Printf(s string, i ...interface{}) {
+	if k.isErrorLvl {
+		k.Logger.Error(fmt.Sprintf(s, i))
+		return
+	}
+
+	k.Logger.Info(fmt.Sprintf(s, i))
+}
+
+func NewKafkaAdapted() *KafkaAdapted {
+	return &KafkaAdapted{
+		Logger: zap.L(),
+	}
+}
+
+func (k *KafkaAdapted) WithServiceName(name string) *KafkaAdapted {
+	k.Logger = zap.L().Named(name)
+	return k
+}
+
+func (k *KafkaAdapted) ForErrors() *KafkaAdapted {
+	k.isErrorLvl = true
+	return k
+}
